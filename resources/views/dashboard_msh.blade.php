cd4328dd43e7662c135f63ca72e21d4dcc224ab0
rhecustein <bintangwijaya18@gmail.com>
2024-06-13 10:36:31 +0700

    66

---
+++ resources/views/dashboard_msh.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('page-heading')
+<div class="page-heading">
+	<div class="page-title">
+		<div class="row">
+			<div class="col-12 col-md-6 order-md-1 order-last">
+				<h3>Dashboard</h3>
+				<p class="text-subtitle text-muted">Dashboard.</p>
+			</div>
+			<div class="col-12 col-md-6 order-md-2 order-first">
+				<nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+					<ol class="breadcrumb">
+						<li class="breadcrumb-item">
+							<a href="{{ route('dashboard') }}">Dashboard</a>
+						</li>
+					</ol>
+				</nav>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
+
+@section('content')
+contect
+@endsection
+
+@pushOnce('scripts')
+<script src="{{ asset('extensions/apexcharts/apexcharts.min.js') }}"></script>
+@include('script')
+@endPushOnce
