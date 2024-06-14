2837f43fc8590a00789be57868efcb0e504aa074
ryugenxd <anonimous7770@gmail.com>
2024-06-15 06:44:46 +0800

    rafactor: view dashboard for student

--- resources/views/dashboard_msh.blade.php
+++ resources/views/dashboard_msh.blade.php
@@ -7,8 +7,7 @@
 	<div class="page-title">
 		<div class="row">
 			<div class="col-12 col-md-6 order-md-1 order-last">
-				<h3>Dashboard</h3>
-				<p class="text-subtitle text-muted">Dashboard.</p>
+				<h3>Halo , {{Auth::user()->name}}</h3>
 			</div>
 			<div class="col-12 col-md-6 order-md-2 order-first">
 				<nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
